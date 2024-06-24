0805580db2ed7ad29b5449fae60e5f16cfa0b67e
ITonlinelearning-code <167775962+ITonlinelearning-code@users.noreply.github.com>
2024-06-24 16:01:11 +0200

    Add files via upload

---
+++ JavaScript_02_02_Change_HTML_With_JavaScript/script.js
@@ -0,0 +1,6 @@
+function changeHeadingText() {
+    // Target the id of the HTML heading element
+    var headingElement = document.getElementById("myHeading");
+    // Assign new text to the HTML heading element
+    headingElement.textContent = "New Heading Text";
+}
